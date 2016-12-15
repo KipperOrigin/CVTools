f0ae4cf509c1e84741179b8ef18cb210feee4bc0
FrediW <frediw@fsmail.de>
2016-12-15 21:46:21 +0100

    Fixed and improved sign text search

--- src/main/java/org/cubeville/commons/utils/BlockGetter.java
+++ src/main/java/org/cubeville/commons/utils/BlockGetter.java
@@ -9,62 +9,57 @@ import org.bukkit.block.Block;
 
 public class BlockGetter {
 	
-	public static List<Block> getBlocksInRadius(Location loc, int radius) {
-		Location loc1 = new Location(loc.getWorld(), loc.getBlockX() - radius, loc.getBlockY() - radius, loc.getBlockZ() - radius);
-		Location loc2 = new Location(loc.getWorld(), loc.getBlockX() + radius, loc.getBlockY() + radius, loc.getBlockZ() + radius);
+    public static List<Block> getBlocksInRadius(Location loc, int radius) {
+        Location loc1 = new Location(loc.getWorld(), loc.getBlockX() - radius, loc.getBlockY() - radius, loc.getBlockZ() - radius);
+        Location loc2 = new Location(loc.getWorld(), loc.getBlockX() + radius, loc.getBlockY() + radius, loc.getBlockZ() + radius);
 		
-		List<Block> blocks = getBlocksInCube(loc1, loc2);
-		List<Block> newBlocks = new ArrayList<>();
+        List<Block> blocks = getBlocksInCube(loc1, loc2);
+        List<Block> newBlocks = new ArrayList<>();
 		
-		for(Block block: blocks) {
-			if (block.getLocation().distance(loc) < radius)
-				newBlocks.add(block);
-		}
-		
-		return newBlocks;
-	}
+        for(Block block: blocks) {
+            if (block.getLocation().distance(loc) < radius)
+                newBlocks.add(block);
+        }
+
+        return blocks;
+    }
 	
-	public static List<Block> getBlocksInCube(Location loc1, Location loc2) {
-		List<Block> blocks = new ArrayList<>();
+    public static List<Block> getBlocksInCube(Location loc1, Location loc2) {
+        List<Block> blocks = new ArrayList<>();
 		
-		for (int x = loc1.getBlockX(); x <= loc2.getBlockX(); x++) {
-			for (int y = loc1.getBlockY(); y <= loc2.getBlockY(); y++) {
-				for (int z = loc1.getBlockZ(); z <= loc2.getBlockZ(); z++) {
-					blocks.add(new Location(loc1.getWorld(),x,y,z).getBlock());
-				}
-			}
-		}
+        for (int x = loc1.getBlockX(); x <= loc2.getBlockX(); x++) {
+            for (int y = loc1.getBlockY(); y <= loc2.getBlockY(); y++) {
+                for (int z = loc1.getBlockZ(); z <= loc2.getBlockZ(); z++) {
+                    blocks.add(new Location(loc1.getWorld(),x,y,z).getBlock());
+                }
+            }
+        }
 		
-		return blocks;
-	}
+        return blocks;
+    }
 	
-	public static List<Block> getBlocksInRadiusByType(Location loc, int radius, Material... mats) {
-		List<Block> blocks = getBlocksInRadius(loc, radius);
+    public static List<Block> getBlocksInRadiusByType(Location loc, int radius, Material... mats) {
+        List<Block> blocks = getBlocksInRadius(loc, radius);
 		
-		return getBlocksByType(blocks, mats);
-	}
+        return getBlocksByType(blocks, mats);
+    }
 	
-	public static List<Block> getBlocksInCubeByType(Location loc1, Location loc2, Material... mats) {
-		List<Block> blocks = getBlocksInCube(loc1, loc2);
+    public static List<Block> getBlocksInCubeByType(Location loc1, Location loc2, Material... mats) {
+        List<Block> blocks = getBlocksInCube(loc1, loc2);
 		
-		return getBlocksByType(blocks, mats);
-	}
+        return getBlocksByType(blocks, mats);
+    }
 	
-	public static List<Block> getBlocksByType(List<Block> blocks, Material... mats) {
-		List<Block> newBlocks = new ArrayList<>();
+    public static List<Block> getBlocksByType(List<Block> blocks, Material... mats) {
+        List<Block> newBlocks = new ArrayList<>();
 		
-		for(Block block: blocks) {
-			boolean isMat = false;
-			for(Material mat: mats) {
-				isMat = false;
-				if(block.getType() == mat)
-					isMat = true;
-			}
-			if (isMat)
-				newBlocks.add(block);
-		}
-		
-		return newBlocks;
-	}
+        for(Block block: blocks) {
+            for(Material mat: mats) {
+                if(block.getType() == mat) newBlocks.add(block);
+            }
+        }
+        
+        return newBlocks;
+    }
 
 }
--- src/main/java/org/cubeville/cvtools/commands/other/CheckSign.java
+++ src/main/java/org/cubeville/cvtools/commands/other/CheckSign.java
@@ -1,5 +1,6 @@
 package org.cubeville.cvtools.commands.other;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -17,37 +18,40 @@ import org.cubeville.commons.utils.Colorize;
 
 public class CheckSign extends Command {
 
-	public CheckSign() {
-		super("checksign");
-		addBaseParameter(new CommandParameterInteger());
-		addBaseParameter(new CommandParameterString());
+    public CheckSign() {
+        super("checksign");
+        addBaseParameter(new CommandParameterInteger());
+        addBaseParameter(new CommandParameterString());
+    }
+
+    @Override
+    public void execute(Player player, Set<String> flags, Map<String, Object> parameters, List<Object> baseParameters)
+        throws CommandExecutionException {
+
+        List<Block> signs = BlockGetter.getBlocksInRadiusByType(player.getLocation(), (int) baseParameters.get(0), Material.SIGN_POST, Material.WALL_SIGN);
+        CharSequence cs = (String) baseParameters.get(1);
+        int amount = 0;
+        List<String> signlist = new ArrayList<>();
+        for (Block sign: signs) {
+            boolean contains = false;
+            Sign signState = (Sign) sign.getState();
+            String[] lines = signState.getLines();
+            String lineCon = "";
+            for (String line: lines) {
+                if (line.contains(cs)) 
+                    contains = true;
+                if(lineCon.length() > 0) lineCon += " ";
+                lineCon += line;
+            }
+            if (contains) {
+                amount += 1;
+                signlist.add(sign.getLocation().getBlockX() + "/" + sign.getLocation().getBlockY() + "/" + sign.getLocation().getBlockZ() + ": " + lineCon);
+            }
 	}
-
-	@Override
-	public void execute(Player player, Set<String> flags, Map<String, Object> parameters, List<Object> baseParameters)
-			throws CommandExecutionException {
-
-		List<Block> signs = BlockGetter.getBlocksInRadiusByType(player.getLocation(), (int) baseParameters.get(0), Material.SIGN, Material.WALL_SIGN);
-		CharSequence cs = (String) baseParameters.get(1);
-		int amount = 0;
-		String color = "&c";
-
-		for (Block sign: signs) {
-			boolean contains = false;
-			Sign signState = (Sign) sign.getState();
-			String[] lines = signState.getLines();
-			for (String line: lines) {
-				if (line.contains(cs)) 
-					contains = true;
-			}
-			if (contains)
-				amount += 1;
-		}
-		
-		if (amount > 0)
-			color = "&a";
-		
-		player.sendMessage(Colorize.addColor(color + amount + " sign(s) contain the string &6" + (String) baseParameters.get(1)));
-	}
-
+        
+        player.sendMessage(Colorize.addColor((amount > 0 ? "&a" : "&c") + amount + " sign(s) contain the string &6" + (String) baseParameters.get(1)) + (amount > 0 ? ":" : "."));
+        for(String s: signlist) {
+            player.sendMessage(s);
+        }
+    }
 }
